42f5554362d665b3637d6fecad965c0cedcbdcc1
edson <edsonfire@gmail.com>
2023-03-16 00:56:54 -0300

    aswre

---
+++ src/app/routes/solicitacao/solicitacao.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-solicitacao',
+  templateUrl: './solicitacao.component.html',
+  styleUrls: ['./solicitacao.component.css']
+})
+export class SolicitacaoComponent {
+
+}
